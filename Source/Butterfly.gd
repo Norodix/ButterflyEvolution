c10a76197ae830a1391d3655a7471be7fb8031f0
Norodix <peceli.bence@gmail.com>
2022-06-02 08:27:49 +0200

    Butterfly motion and animation syncd, flight tweaked

--- Source/Butterfly.gd
+++ Source/Butterfly.gd
@@ -10,6 +10,9 @@ var flapDir = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite.play("Resting")
+	$AnimatedSprite.speed_scale = 4 / $FlapTimer.wait_time
+	print("Flap speed scale: ", $AnimatedSprite.speed_scale)
 	print("Butterfly ready")
 	pass # Replace with function body.
 
@@ -20,19 +23,39 @@ func _physics_process(delta):
 	var dir = get_direction()
 	if canFlap() and dir.length_squared() != 0:
 		flapDir = dir
-		DDD.DrawRay(self.global_position, flapDir*50, Color(0, 0, 1), 2)
+		if flapDir.x > 0:
+			$AnimatedSprite.flip_h = false
+		if flapDir.x < 0:
+			$AnimatedSprite.flip_h = true
+		#DDD.DrawRay(self.global_position, flapDir*50, Color(0, 0, 1), 2)
 		$FlapTimer.start()
+		$AnimatedSprite.frame = 0
+		$AnimatedSprite.play("Flap")
 	#If flapping go to direction
 	if isFlapping():
 		velocity = lerp(velocity, flapDir*flapSpeed, flapLerpCoefficient)
 		pass
+	else:
+		$AnimatedSprite.stop()
 	
 	#self.global_position += velocity * delta
-	move_and_slide(velocity, Vector2.UP, true, 4, deg2rad(80))
+	#velocity = move_and_slide(velocity, Vector2.UP, true, 4, deg2rad(80))
+	var collision : KinematicCollision2D = move_and_collide(velocity * delta, true, true, true)
+	if collision:
+		if collision.normal.dot(Vector2.DOWN) > 0: # hit a ceiling
+			velocity = velocity - collision.normal * collision.normal.dot(velocity)
+		elif collision.normal.dot(Vector2.DOWN) < - cos(deg2rad(85)): #hit floor
+			velocity = Vector2.ZERO
+			if $AnimatedSprite.playing == false: #Only change to rest if flap is done
+				$AnimatedSprite.play("Rest")
+				print("Rest")
+			pass
+	move_and_collide(velocity * delta)
 	
 func get_direction() -> Vector2 :
 	var dir = Vector2(0, 0)
-	var R_vec = Vector2(cos(deg2rad(30)), - sin(deg2rad(30)))
+	var sideFlapDeg = 10
+	var R_vec = Vector2(cos(deg2rad(sideFlapDeg)), - sin(deg2rad(sideFlapDeg)))
 	var L_vec = Vector2( - R_vec.x, R_vec.y)
 	var U_vec = Vector2(0, -1)
 	var r = int(Input.is_action_pressed("ui_right"))
