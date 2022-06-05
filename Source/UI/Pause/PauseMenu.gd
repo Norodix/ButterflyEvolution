fe4ca846bfb8e7610996f2eb85e3d3bd2ec6b947
Norodix <peceli.bence@gmail.com>
2022-06-05 19:18:39 +0200

    Tutorial added, UI updates

--- Source/UI/Pause/PauseMenu.gd
+++ Source/UI/Pause/PauseMenu.gd
@@ -12,13 +12,23 @@ func _process(delta):
 		
 	self.visible = get_tree().paused
 	Engine.time_scale = float(int(not get_tree().paused))
-
+	
+func _input(event):
+	var a = (event is InputEventMouseButton) \
+		or (event is InputEventJoypadButton) \
+		or (event is InputEventKey)
+	if a:
+		$Tutorial.visible = false
 
 func _on_Resume_pressed():
+	$Tutorial.visible = not $Tutorial.visible
 	get_tree().paused = false
 	pass # Replace with function body.
 
-
 func _on_Exit_pressed():
 	get_tree().quit()
+	pass # Replace with function body.
+
+func _on_Tutorial_pressed():
+	$Tutorial.visible = true
 	pass # Replace with function body.
