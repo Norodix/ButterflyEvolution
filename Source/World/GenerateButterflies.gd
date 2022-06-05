1ec032048a86148f52fb9485ca88757bb1b7a472
Norodix <peceli.bence@gmail.com>
2022-06-05 07:41:55 +0200

    Hue randomization includes nice color distribution

--- Source/World/GenerateButterflies.gd
+++ Source/World/GenerateButterflies.gd
@@ -5,12 +5,20 @@ var butterflies_per_path : float = 3.0
 onready var butterfly = preload("res://Butterfly_AI/PathFollowButterfly.tscn")
 
 func _ready():
+	randomize()
 	var children = get_children()
+	#Create a shuffled hue array
+	var colorNum = children.size() * butterflies_per_path
+	var colors = []
+	for i in range(colorNum):
+		colors.append(float(i) / float(colorNum))
+	colors.shuffle()
+	
 	for child in children:
 		for i in range(butterflies_per_path):
 			var bf = butterfly.instance()
 			butterflies.append(bf)
-			bf.hue = randf()
+			bf.hue = colors.pop_back()
 			child.add_child(bf)
 			bf.unit_offset = i / butterflies_per_path
 		
